953cf46895b2ba331b24a3047fcabf7449216d5d
Don Stewart <dons@galois.com>
2008-08-09 23:18:19 +0100

    typo

--- Main.hs
+++ Main.hs
@@ -684,7 +684,7 @@ emptyPkgBuild =
     -- everything depends on ghc and Cabal 1.4.x
     , arch_makedepends = ArchList
         [(ArchDep (Dependency "ghc"    AnyVersion))
-        ,(ArchDep (Dependency "Cabal" (ThisVersion (Version  [1,4,0,1] []))))
+        ,(ArchDep (Dependency "haskell-cabal" (ThisVersion (Version  [1,4,0,1] []))))
         ]
 
         -- makedepends=('ghc>=6.6') ?
