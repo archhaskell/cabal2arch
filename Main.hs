587cd04937b11a61f36a055c82e93855d65b6df3
Rémy Oudompheng <oudomphe@math.unice.fr>
2011-01-01 23:17:23 +0100

    Move several utility functions to a separate module.

--- Main.hs
+++ Main.hs
@@ -29,7 +29,6 @@ import Distribution.ArchLinux.SystemProvides
 import Distribution.ArchLinux.HackageTranslation
 
 import Control.Monad
-import Control.Concurrent
 import qualified Control.Exception as CE
 
 import Data.List
@@ -41,13 +40,13 @@ import Paths_cabal2arch
 import Data.Version (showVersion)
 
 import System.Directory
-import System.Environment
 import System.Exit
 import System.FilePath
 import System.IO
 import System.Process hiding(cwd)
 
 import System.Console.CmdArgs
+import Cabal2Arch.Util
 
 data CmdLnArgs
     = CmdLnConvertOne { argCabalFile :: String, argCreateTar :: Bool }
@@ -267,52 +266,3 @@ findCabalFile file _cwd tmp = do
                 then die $ "directory doesn't exist: " ++ show dir
                 else findPackageDesc dir
 
-------------------------------------------------------------------------
--- Some extras
---
-
-die :: String -> IO a
-die s = do
-    hPutStrLn stderr $ "cabal2pkg:\n" ++ s
-    exitWith (ExitFailure 1)
-
--- Safe wrapper for getEnv
-getEnvMaybe :: String -> IO (Maybe String)
-getEnvMaybe _name = CE.handle ((const :: a -> CE.SomeException -> a) $ return Nothing) (Just `fmap` getEnv _name)
-
-------------------------------------------------------------------------
-
---
--- Strict process reading
---
-myReadProcess :: FilePath                              -- ^ command to run
-            -> [String]                              -- ^ any arguments
-            -> String                                -- ^ standard input
-            -> IO (Either (ExitCode,String,String) String)  -- ^ either the stdout, or an exitcode and any output
-
-myReadProcess cmd _args input = CE.handle (return . handler) $ do
-    (inh,outh,errh,pid) <- runInteractiveProcess cmd _args Nothing Nothing
-
-    output  <- hGetContents outh
-    outMVar <- newEmptyMVar
-    _ <- forkIO $ (CE.evaluate (length output) >> putMVar outMVar ())
-
-    errput  <- hGetContents errh
-    errMVar <- newEmptyMVar
-    _ <- forkIO $ (CE.evaluate (length errput) >> putMVar errMVar ())
-
-    when (not (null input)) $ hPutStr inh input
-    takeMVar outMVar
-    takeMVar errMVar
-    ex <- CE.catch (waitForProcess pid) ((const :: a -> CE.SomeException -> a) $ return ExitSuccess)
-    hClose outh
-    hClose inh          -- done with stdin
-    hClose errh         -- ignore stderr
-
-    return $ case ex of
-        ExitSuccess   -> Right output
-        ExitFailure _ -> Left (ex, errput, output)
-
-    where
-        handler (ExitFailure e) = Left (ExitFailure e,"","")
-        handler e               = Left (ExitFailure 1, show e, "")
